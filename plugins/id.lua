904d838f7c3cb08965b7819a0c441bee33178733
Electrovirus <E.electrovirus@gmail.com>
2016-06-20 22:35:55 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,4 +1,4 @@
-#addplug local function run(msg, matches)
+local function run(msg, matches)
      if matches[1]:lower() == '!id' and is_sudo(msg) then
   text = "!id"
   reply_msg(msg.id, text, ok_cb, false)
@@ -9,4 +9,4 @@ return {
        "^[!]id$",
   },
   run = run,
-} 333
+}
