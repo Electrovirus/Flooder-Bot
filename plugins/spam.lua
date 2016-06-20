099dd67b40b65459480fc48c171980f8e36d36c0
Electrovirus <E.electrovirus@gmail.com>
2016-06-20 22:11:58 +0430

    Flooder

---
+++ plugins/spam.lua
@@ -0,0 +1,19 @@
+local function run(msg, matches)
+
+  local receiver = get_receiver(msg)
+    if matches[1] == "spam" and is_sudo(msg) then
+    local num = matches[2]
+     local text = matches[3]
+        for i=1,num do
+            send_large_msg(receiver, text)
+        end
+  end
+end
+ 
+return {
+  patterns = {
+  "^[#!/](spam) (%d+) (.*)$",
+  "^[Ss](pam) (%d+) (.*)$",
+  },
+  run = run,
+} superspam
